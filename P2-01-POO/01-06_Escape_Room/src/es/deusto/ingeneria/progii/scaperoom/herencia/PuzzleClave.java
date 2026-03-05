ddfa419104be5c82c6d4ee094260ded34dee06e2
bosanz <borja.sanz@deusto.es>
2026-03-05 09:55:54 +0100

    Scape room con herencia.

---
+++ P2-01-POO/01-06_Escape_Room/src/es/deusto/ingeneria/progii/scaperoom/herencia/PuzzleClave.java
@@ -0,0 +1,24 @@
+package es.deusto.ingeneria.progii.scaperoom.herencia;
+
+
+import java.util.Random;
+
+public class PuzzleClave extends Puzzle {
+    private final String solucionEsperada;
+
+    public PuzzleClave(String id, String enunciado, boolean obligatorio, String solucionEsperada) {
+        super(id, enunciado, obligatorio);
+        this.solucionEsperada = solucionEsperada;
+    }
+
+    @Override
+    protected boolean resolver(String solucion, Random rnd) {
+        if (solucion == null) return false;
+        return solucionEsperada != null && solucionEsperada.equals(solucion);
+    }
+
+    @Override
+    public String getTipo() {
+        return "CLAVE";
+    }
+}
---
+++ P2-01-POO/01-06_Escape_Room/src/es/deusto/ingeneria/progii/scaperoom/herencia/PuzzleProbabilistico.java
@@ -0,0 +1,24 @@
+package es.deusto.ingeneria.progii.scaperoom.herencia;
+
+
+import java.util.Random;
+
+public class PuzzleProbabilistico extends Puzzle {
+    private final int probExitoPorIntento; // 0..100
+
+    public PuzzleProbabilistico(String id, String enunciado, boolean obligatorio, int probExitoPorIntento) {
+        super(id, enunciado, obligatorio);
+        this.probExitoPorIntento = clamp(probExitoPorIntento, 0, 100);
+    }
+
+    @Override
+    protected boolean resolver(String solucion, Random rnd) {
+        int roll = rnd.nextInt(100) + 1; // 1..100
+        return roll <= probExitoPorIntento;
+    }
+
+    @Override
+    public String getTipo() {
+        return "PROB";
+    }
+}
